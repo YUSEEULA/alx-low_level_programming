f7816df877ea8d055d282293303dfb729ad41358
YUSEEULA <yousraelh12@gmail.com>
2023-03-16 13:06:16 -0700

    using.puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void)
+int main( )
 {
-puts("Programming is like building a multilingual puzzle")
-	return0;
+	puts(""Programming is like building a multilingual puzzle");
+
+	return 0;
 }
