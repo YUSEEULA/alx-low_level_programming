10576ee16f9e1f282ea7262e54e39387f308bef8
YUSEEULA <yousraelh12@gmail.com>
2023-03-18 07:07:23 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /*
- * main - Write a C program that prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types on the computer
  *
  * Return: always 0
  */
-int main()
+int main(void)
 {
-    char myChar;
-    int myInt;
-    long int myLongInt;
-    long long int myLongLongInt;
-    float myFloat;
+	char myChar;
+	int my Int;
+	long int myLongInt;
+	long long int myLongLongInt;
+	float myFloat;
     {
-        printf("size of a char: %d byte(s)\n",sizeof(myChar));
-        printf("size of an int: %d byte(s)\n",sizeof(myInt));
-        printf("size of a long int: %d byte(s)\n",sizeof(myLongInt));
-        printf("size of a long long int: %d byte(s)\n",sizeof(myLongLongInt));
-        printf("size of a float: %d byte(s)\n",sizeof(myFloat));
+	    printf("size of a char: %d byte(s)\n",sizeof(myChar));
+	    printf("size of an int: %d byte(s)\n",sizeof(myInt));
+	    printf("size of a long int: %d byte(s)\n",sizeof(myLongInt));
+	    printf("size of a long long int: %d byte(s)\n",sizeof(myLongLongInt));
+	    printf("size of a float: %d byte(s)\n",sizeof(myFloat));
+
     }
   
     return 0;
