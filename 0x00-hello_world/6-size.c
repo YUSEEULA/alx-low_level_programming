cfe20e5940c8fdac7ffba768437a50c89f97c433
YUSEEULA <yousraelh12@gmail.com>
2023-03-18 13:28:59 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-char myChar;
-int myInt;
-long int myLongInt;
-long long int myLongLongInt;
-float myFloat;
-
-printf("size of a char: %ld byte(s)\n", sizeof(myChar));
-printf("size of an int: %ld byte(s)\n", sizeof(myInt));
-printf("size of a long int: %ld byte(s)\n", sizeof(myLongInt));
-printf("size of a long long int: %ld byte(s)\n", sizeof(myLongLongInt));
-printf("size of a float: %ld byte(s)\n", sizeof(myFloat));
-return 0;
+	char myChar;
+	int myInt;
+	long int myLongInt;
+	long long int myLongLongInt;
+	float myFloat;
+	{
+		printf("size of a char: %ld byte(s)\n", sizeof(myChar));
+		printf("size of an int: %ld byte(s)\n", sizeof(myInt));
+		printf("size of a long int: %ld byte(s)\n", sizeof(myLongInt));
+		printf("size of a long long int: %ld byte(s)\n", sizeof(myLongLongInt));
+		printf("size of a float: %ld byte(s)\n", sizeof(myFloat));
+	}
+	return 0;
 }
