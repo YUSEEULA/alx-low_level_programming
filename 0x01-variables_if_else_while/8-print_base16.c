6de47333a5e583305d818a2d4af064abda76430e
YUSEEULA <yousraelh12@gmail.com>
2023-03-19 08:20:16 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	{
+		for (a = 0; a <= 9; a++)
+			putchar(a + '0');
+		for (b = 'a'; b <= 'f'; b++)
+			putchar(b);
+	}
+
+	putchar('\n');
+	return (0);
+}
