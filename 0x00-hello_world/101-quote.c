0c25af62334cd8bdd2397ce4165681e80c43906f
YUSEEULA <yousraelh12@gmail.com>
2023-03-18 17:47:11 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,10 @@
  *
  * Return: Always 1
  */
-int main(void)
+int main()
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    int STDOUT_FILENO;
+    write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+    return 0;
 }
