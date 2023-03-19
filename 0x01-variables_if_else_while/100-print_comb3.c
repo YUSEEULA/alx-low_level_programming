13a882850103a4ed8749d6760e03c62e4fa81681
YUSEEULA <yousraelh12@gmail.com>
2023-03-19 11:45:09 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,15 @@ int main(void)
 	int X;
 	int Y;
 
-	for (X = 0; X <= 10; X++)
+	for (X = 0; X < 9; X++)
 	{
 		for (Y = X + 1; Y < 10; Y++)
 		{
 			putchar(X + '0');
 			putchar(Y + '0');
-			Y++;
 			putchar(',');
 			putchar(' ');
 		}
-		X++;
 	}
 	putchar('\n');
 	return (0);
