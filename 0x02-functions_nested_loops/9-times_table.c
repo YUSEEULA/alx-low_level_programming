472bbd6735d5a4dd1cc97bf7346eefa437ad5ca3
YUSEEULA <yousraelh12@gmail.com>
2023-03-21 12:57:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,16 +17,21 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		for (b = 0; b <= 9; b++)
 		{
 			r = (a * b);
 			if ((r / 10) > 0)
+			{
 				_putchar((r / 10) + '0');
+			}
 			else
 			{
 				_putchar(' ');
-				_putchar((r % 10) + '0');
 			}
+			_putchar((r % 10) + '0');
 
 			if (b < 9)
 			{
