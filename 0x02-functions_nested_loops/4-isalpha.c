670ef546ce4004b4533ac16cd601398527bf3b55
YUSEEULA <yousraelh12@gmail.com>
2023-03-21 07:12:59 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha -  function that checks for alphabetic character
- * @c character to be verified
+ * @c: character to be verified
  * Returns 1 if c is a letter, lowercase or uppercase
  * Returns 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
