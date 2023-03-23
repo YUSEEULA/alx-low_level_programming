d658d4d79ee22526fa1c87430ebc98d826976b87
YUSEEULA <yousraelh12@gmail.com>
2023-03-23 03:51:06 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit -  function that checks for a digit
+ * @c: digit to be verified
+ * Return : 1 if c is a digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
