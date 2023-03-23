ae14d0910f2e41dd9a315033a2d418db998f1eda
YUSEEULA <yousraelh12@gmail.com>
2023-03-23 11:28:39 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,17 +4,18 @@
 * @n: The number to print
 *
 */
+
 void print_number(int n)
 {
-	unsigned int num = n;
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-	if (num > 9)
-	{
-		print_number(num / 10);
-	}
-	_putchar(num % 10 + '0');
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if (num > 9)
+{
+print_number(num / 10);
+}
+_putchar(num % 10 + '0');
 }
