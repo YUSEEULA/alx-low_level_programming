ad70933308ce488144f59e874930ab8662fcb818
YUSEEULA <yousraelh12@gmail.com>
2023-03-25 12:08:41 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print the largest factor of the number 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int div=2, F;
+
+	while(n!=0)
+	{
+		if(n % div !=0)
+			div = div + 1;
+		else
+		{
+			F = n;
+			n = n / div;
+			if(n == 1)
+			{
+				printf("%ld", F);
+				break;
+			}
+		}
+	}
+	return (0);
+}
