e4dabb1eb535ef94d4e89aaa2f47f9fceec82b99
YUSEEULA <yousraelh12@gmail.com>
2023-03-27 16:27:22 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,11 @@ void rev_string(char *s)
 	int len = strlen(s);
 	int a;
 
-	for (a = 0; a < len/2; a++)
+	for (a = 0; a < len / 2; a++)
 	{
 		char c = s[a];
-		s[a] = s[len-1-a];
-		s[len-1-a] = c;
+
+		s[a] = s[len - 1 - a];
+		s[len - 1 - a] = c;
 	}
 }
