f2e6af20af3e77d4f3f9d403d8809c430a999615
YUSEEULA <yousraelh12@gmail.com>
2023-03-27 15:48:04 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: the string
+ *
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
