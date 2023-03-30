8e14b8e6d0e4ec159d5c5068b909140945343550
YUSEEULA <yousraelh12@gmail.com>
2023-03-30 12:30:41 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *leet - function that encodes a string into 1337.
+ * @s: string
+ *
+ * Return: the answer
+ */
+char *leet(char *s)
+{
+	int a, b;
+	char alpha[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char numbers[] = {4, 3, 0, 7, 1};
+
+	for (a = 0; s[a] != '\0'; ++a)
+	{
+		for (b = 0; alpha[b] != '\0'; b++)
+		{
+			if (s[a] == alpha[b])
+				s[a] = numbers[b / 2];
+		}
+	}
+	return (s);
+}
