ea4e407f3394eade5864976f03c068b51d1416fd
YUSEEULA <yousraelh12@gmail.com>
2023-03-30 07:19:37 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - function that capitalizes all words of a string
+ * @s: the string
+ * Separators of words: space, tabulation
+ * new line:, ,, ;, ., !, ?, ", (, ), {, and }.
+ *
+ * Return: the answer
+ */
+
+char *cap_string(char *s)
+{
+	int letter = 0;
+
+	while (s[letter] != '\0')
+	{
+		if (s[letter] >= 97 && s[letter] <= 122)
+		{
+			s[letter] -= 32;
+		}
+		if (s[letter] == ' ' || s[letter] == ',' || s[letter] == ';'
+			|| s[letter] == '.' || s[letter] == '!' || s[letter] == '?'
+			|| s[letter] == '"' || s[letter] == '(' || s[letter] == ')'
+			|| s[letter] == '{' || s[letter] == '}' || s[letter] == '\t'
+			|| s[letter] == '\n')
+		{
+			if (s[letter + 1] >= 97 && s[letter + 1] <= 122)
+				s[letter + 1] -= 32;
+		}
+		letter++;
+	}
+	return (0);
+}
