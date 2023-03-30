05edd4207c44eeae4c130f8ea081dde86bf33cad
YUSEEULA <yousraelh12@gmail.com>
2023-03-30 14:43:05 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,13 +10,13 @@ void print_number(int n)
 {
 	unsigned int num;
 
+	num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -n;
 	}
-
-	num = n;
 
 	if (num / 10)
 		print_number(num / 10);
