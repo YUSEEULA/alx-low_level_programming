a7f8f9bdca73e3dbb26d74662171e2d7d8d93f24
YUSEEULA <yousraelh12@gmail.com>
2023-04-06 06:24:49 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,9 +17,9 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s2 == '*')
+	if (*s2 == '*' && *(s2 + 1) != 0 && *s2 == '\0')
 	{
-		return (wildcmp(s1, s2 + 1));
+		return (wildcmp(0));
 	}
 	if (*s2 == '*')
 	{
