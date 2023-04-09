fb6e323f3f20f819bc34e4439fa49f368ea87d97
YUSEEULA <yousraelh12@gmail.com>
2023-04-09 06:21:35 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point , print name
+ * @argc: is a count of the arguments supplied to the program
+ * @argv: is an array of pointers to the strings
+ * Return: return the answer
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d %s\n", i, argv[i]);
+	}
+	return (0);
+}
