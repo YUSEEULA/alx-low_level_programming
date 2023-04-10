e83cc3fb36deb4a1246911d1faaa89a691fff81f
YUSEEULA <yousraelh12@gmail.com>
2023-04-10 06:26:16 -0700

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,20 +20,21 @@ int main(int argc, char *argv[])
 	{
 		printf("0\n");
 	}
-
-	for (num1 = 1; num1 < argc; num1++)
+	else 
 	{
-		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		for (num1 = 1; num1 < argc; num1++)
 		{
-			if (!isdigit(argv[num1][num2]))
+			for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(argv[num1][num2]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 		}
+		sum = sum + atoi(argv[num1]);
 	}
-	sum = sum + atoi(argv[num1]);
-
 	printf("%d\n", sum);
 	return (0);
 }
