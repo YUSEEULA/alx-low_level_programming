981cc9691dff77487a8017bf74aebb8e5c8fe433
YUSEEULA <yousraelh12@gmail.com>
2023-04-11 14:38:27 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,17 +13,26 @@ char *str_concat(char *s1, char *s2)
 {
 	int len1 = strlen(s1);
 	int len2 = strlen(s2);
-	int len = len1 + len2;
 	char *res;
 	int a, b;
+
+	a = b = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+	while (s1[a] != '\0')
+	{
+		a++;
+	}
+	while (s2[b] != '\0')
+	{
+		b++;
+	}
 
-	res = (char *) malloc((len + 1) * sizeof(char));
+	res = malloc((a + b + 1) * sizeof(char));
 	if (res == NULL)
 	{
 		return (NULL);
