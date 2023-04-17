3dd6b0587f9e181dc952d732ccb417719d77b00e
YUSEEULA <yousraelh12@gmail.com>
2023-04-17 01:31:32 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - structure that define data type of the dog
  * @name: the name of the dog
@@ -12,4 +12,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
