3fe318777feb678cf93f657ed7816f346741b8fc
YUSEEULA <yousraelh12@gmail.com>
2023-04-21 13:37:31 -0700

    print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,7 +5,6 @@
 /**
  * print_all - function that prints anything
  * @format: list of types of arguments passed to the function
- * Return: the answer
  */
 void print_all(const char * const format, ...)
 {
