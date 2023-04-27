172492e8a4c0af075c965eca273ee0c9778f1d01
YUSEEULA <yousraelh12@gmail.com>
2023-04-27 14:41:34 -0700

    print message before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+void b_main(void);
+
+/**
+ * b_main - function that print a message before the main function
+ *
+ */
+
+void b_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
